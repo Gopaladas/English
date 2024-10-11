993114432bd63b73b997acd9b67653604de42250
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2024-10-11 23:53:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const frontendUrl = "https://english-2kuj-frontend.vercel.app"; // Your frontend
 
 app.use(
   cors({
-    origin: frontendUrl,
+    origin: [frontendUrl],
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
   })
