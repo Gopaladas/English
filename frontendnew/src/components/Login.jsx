a2e1ea388a7824204a123e4b212b06a4f52eb040
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2024-12-10 15:37:02 +0530

    Update Login.jsx

--- frontendnew/src/components/Login.jsx
+++ frontendnew/src/components/Login.jsx
@@ -44,7 +44,7 @@ const LoginForm = () => {
     dispatch(startloading());
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/admin/adminlogin",
+        "https://english-ashy.vercel.app/?vercelToolbarCode=RTHpJKORBufMuCm/adminlogin",
         logindata,
         {
           withCredentials:true
@@ -74,7 +74,7 @@ const LoginForm = () => {
       dispatch(startloading());
       try {
         const response = await axios.get(
-          "http://localhost:3000/api/admin/isLoggedIn",
+          "https://english-ashy.vercel.app/?vercelToolbarCode=RTHpJKORBufMuCm/isLoggedIn",
           {
           withCredentials:true
         }
@@ -85,7 +85,7 @@ const LoginForm = () => {
         if (response?.data && response?.data?.user) {
           if (response?.data?.user?.role === "admin") {
             const res = await axios.get(
-              `http://localhost:3000/api/admin/admindata/:${response?.data?.user?.id}`,
+              `https://english-ashy.vercel.app/?vercelToolbarCode=RTHpJKORBufMuCm/admindata/:${response?.data?.user?.id}`,
              {
           withCredentials:true
         }
@@ -99,7 +99,7 @@ const LoginForm = () => {
           } else {
             // `https://english-ivory.vercel.app/api/faculty/eachfacultydetails/${response.data.user.id}`
             const res = await axios.get(
-              `http://localhost:3000/api/faculty/eachfacultydetails/${response.data.user.id}`,
+              `https://english-ashy.vercel.app/?vercelToolbarCode=RTHpJKORBufMuCm/eachfacultydetails/${response.data.user.id}`,
                 {
           withCredentials:true
         }
