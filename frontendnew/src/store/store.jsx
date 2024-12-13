31a155423dcf638df397ccceccc99a1a3cebf175
Pavankumar <114066364+Gopaladas@users.noreply.github.com>
2024-12-14 03:58:26 +0530

    Update store.jsx

--- frontendnew/src/store/store.jsx
+++ frontendnew/src/store/store.jsx
@@ -4,6 +4,8 @@ import authReducer from "./auth/authSlice.jsx";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
+
+
 const persistConfig = {
   key: "root",
   storage,
